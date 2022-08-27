5224a118aa26487e850810796f2dcd8e7cc65f6e
IliescuM <mihnea.iliescu06@gmail.com>
2022-08-27 05:16:13 +0300

    done

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -23,21 +23,38 @@ class App extends React.Component {
         // THIS IS THE ONLY TIME we do direct assignment
         // to this.state
         this.state = { lat: null };
-    }
-
-    // React says we have to define render!!
-
-    render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 // we called setstate !!!!
                 this.setState({ lat: position.coords.latitude });
             },
-            (err) => console.log(err)
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
         );
-        return (
-            <div>Latitude: {this.state.lat} </div>
-        )
+    }
+    componentDidMount() {
+        console.log('My component was rendered to the screen');
+    }
+    componentDidUpdate() {
+        console.log('My component was just updated - it rerendered')
+    }
+
+
+
+
+    // React says we have to define render!!
+
+    render() {
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return <div>Loading!</div>
+
     }
 }
 
